b59d18883fe05c74d96a1556ad1a230fe4f3a3ec
zigaosolin <ziga.osolin@gmail.com>
2019-04-27 09:54:49 +0200

    Added basic movement tracking to array

---
+++ LudumDare44/Assets/Scripts/PointerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PointerState
+{
+    NotPresent,
+    InJumpMovement,
+    InNormalMovement
+}
+
+struct PointerMovementState
+{
+    public PointerState State;
+    public Vector2 PositionScreen;
+}
+
+
+public class PointerMovement : MonoBehaviour
+{
+    private const int NumberOfStatesKept = 120;
+    
+    private List<PointerMovementState> movements;
+
+    [SerializeField] private float jumpMovementThreshold = 10;
+
+    private void Awake()
+    {
+        movements = new List<PointerMovementState>(NumberOfStatesKept + 1);
+        for(int i = 0; i < NumberOfStatesKept; ++i)
+        {
+            movements.Add(new PointerMovementState() { State = PointerState.NotPresent });
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        var movementState = new PointerMovementState()
+        {
+            PositionScreen = Input.mousePosition
+        };
+
+        var prevMovementState = movements[movements.Count - 1];
+        var deltaPosition = movementState.PositionScreen - prevMovementState.PositionScreen;
+        switch (prevMovementState.State)
+        {
+            case PointerState.NotPresent:
+                movementState.State = PointerState.InNormalMovement;
+                break;
+            case PointerState.InNormalMovement:
+            case PointerState.InJumpMovement:
+                bool isFastMovement = deltaPosition.magnitude > jumpMovementThreshold;
+                movementState.State = isFastMovement ? PointerState.InJumpMovement : PointerState.InNormalMovement;             
+                break;
+        }
+
+        // This can be implemented more efficiently by circular buffer
+        // but there is no implementation directly in C#
+        movements.Add(movementState);
+        movements.RemoveAt(0);
+
+
+        
+    }
+}
