a0f149cad7fd7397e6168cc67b4816033777380d
zigaosolin <ziga.osolin@gmail.com>
2019-04-27 10:03:03 +0200

    Added Trace class so we have a controlled way what is output

---
+++ LudumDare44/Assets/Scripts/Trace.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public enum TraceCategory
+{
+    PointerMovement
+}
+
+public static class Trace 
+{
+    public static TraceCategory EnabledMask = TraceCategory.PointerMovement;
+
+    // TODO: strip this code in the end!
+    public static void Info(TraceCategory category, string message)
+    {
+        if (!EnabledMask.HasFlag(category))
+            return;
+
+        Debug.Log("[" + category.ToString() + "]" + message);
+    }
+}
