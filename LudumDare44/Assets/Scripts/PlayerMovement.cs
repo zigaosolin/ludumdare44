2dd998a7e10a4a9c7fcb24e8e78f459e025235a2
zigaosolin <ziga.osolin@gmail.com>
2019-04-27 09:55:08 +0200

    Added player movement component (that does absolutly nothing yet)

---
+++ LudumDare44/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+    }
+}
