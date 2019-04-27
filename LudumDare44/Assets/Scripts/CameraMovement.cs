dafc98c2892cb92a00b11187f70bc01c3bd8753e
zigaosolin <ziga.osolin@gmail.com>
2019-04-27 12:29:05 +0200

    Made prefabs from components for easier editing

---
+++ LudumDare44/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = transform.localPosition;
+        position.y += movementSpeed * Time.deltaTime;
+        transform.localPosition = position;
+    }
+}
